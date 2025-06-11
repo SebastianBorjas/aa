9b7f20f9825dc67d8564f043c2646d94ff99afeb
SebastianBorjas <panfilacaches@gmail.com>
2025-06-11 11:10:21 -0600

    faviconear

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Modalidad Dual - Login</title>
+    <link rel="icon" href="{{ asset('images/lgo.png') }}" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
