751c383b9d30d945410d45435d5fe3591dcb432f
SebastianBorjas <panfilacaches@gmail.com>
2025-06-11 09:55:48 -0600

    Agregar boton inicio

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,12 +12,12 @@
 
   <!-- Header -->
   <header class="bg-[#202c54] shadow-md py-4 px-4 sm:px-6 lg:px-8">
-    <div class="flex flex-col sm:flex-row items-center justify-start space-y-4 sm:space-y-0 sm:space-x-4">
+    <a href="{{ url('/') }}" class="flex flex-col sm:flex-row items-center justify-start space-y-4 sm:space-y-0 sm:space-x-4">
       <img src="{{ asset('images/lgo3.png') }}" alt="Logo" class="w-10 h-10 object-contain">
       <h1 class="text-2xl font-bold text-white">
         @yield('title')
       </h1>
-    </div>
+    </a>
   </header>
 
   <!-- Contenido principal -->
